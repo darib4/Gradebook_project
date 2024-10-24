1e0a8ec3f5c0b9f65f5562f155c116125658f121
Darina Bardarova <118005988+darib4@users.noreply.github.com>
2024-10-24 12:53:46 +0300

    Update Main.java

--- project_db/src/project/Main.java
+++ project_db/src/project/Main.java
@@ -4,25 +4,41 @@ public class Main {
     public static void main(String[] args) {
         GradeBook gradeBook = new GradeBook();
 
+        // Създаване на преподаватели
         Teacher teacher1 = new Teacher("Иван Иванов", "Математика");
         Teacher teacher2 = new Teacher("Петя Петрова", "Информатика");
 
         gradeBook.addTeacher(teacher1);
         gradeBook.addTeacher(teacher2);
 
+        // Създаване на студенти
         Student student1 = new Student("Валентин", "Василев");
         Student student2 = new Student("Иван", "Иванов");
 
         gradeBook.addStudent(student1);
         gradeBook.addStudent(student2);
 
-        gradeBook.addGradeToStudent(student1, new Grade(6, teacher1));
-        gradeBook.addGradeToStudent(student1, new Grade(5, teacher2));
-        gradeBook.addGradeToStudent(student2, new Grade(4, teacher1));
+        // Създаване на курсове
+        Course mathCourse = new Course("Математика");
+        Course informaticsCourse = new Course("Информатика");
 
+        gradeBook.createCourse(mathCourse);
+        gradeBook.createCourse(informaticsCourse);
+
+        // Записване на студентите в курсове
+        gradeBook.enrollStudentInCourse(student1, mathCourse);
+        gradeBook.enrollStudentInCourse(student1, informaticsCourse);
+        gradeBook.enrollStudentInCourse(student2, mathCourse);
+
+        // Добавяне на оценки за студентите
+        gradeBook.addGradeToStudent(student1, new Grade(6, teacher1, "Математика"));
+        gradeBook.addGradeToStudent(student1, new Grade(5, teacher2, "Информатика"));
+        gradeBook.addGradeToStudent(student2, new Grade(4, teacher1, "Математика"));
+
+        // Показване на резултатите
         gradeBook.showStudents();
+        gradeBook.showCourses();
         gradeBook.showGradesForStudent(student1);
         gradeBook.showGradesForStudent(student2);
     }
 }
-
